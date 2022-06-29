29eb4bf6516a69c5d1cb1bc9310b9431f575bb93
keedjk7 <jirapat_kaewsongsang@hotmail.com>
2022-06-29 20:11:02 +0700

    first with open-cv

---
+++ face-detect.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img = cv.imread('picnic.jpg')
+
+face_model = cv.CascadeClassifier('face-detect-model.xml')
+gray_scale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+faces = face_model.detectMultiScale(gray_scale)
+
+for (x,y,w,h) in faces:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,0,250), 2)
+
+cv.imshow('image', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
